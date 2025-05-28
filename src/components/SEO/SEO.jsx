da783f15b2a130bd24199889ffcaf784749dfddd
mahmoud Ad <mahmoodads12@gmail.com>
2025-05-29 00:25:01 +0200

    update SEO

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export const metadata = {
-    title: 'Eclassify - Buy & Sell Marketplace | Find Everything You Need',
-    description: 'Discover Eclassify, your ultimate online marketplace for buying and selling a wide range of products. Enjoy a seamless shopping experience with diverse listings, secure transactions, and excellent customer support. Join Eclassify today and start exploring!',
+    title: 'WynFi - Buy & Sell Marketplace | Find Everything You Need',
+    description: 'Discover WynFi, your ultimate online marketplace for buying and selling a wide range of products. Enjoy a seamless shopping experience with diverse listings, secure transactions, and excellent customer support. Join WynFi today and start exploring!',
     // keywords: "buy and sell marketplace, online marketplace, eclassify, buy and sell online, online shopping, sell products online, buy products online, secure transactions, online listings, customer support, e-commerce, digital marketplace, shop online, sell online, online buying and selling"
 }
 const SEO = () => {
